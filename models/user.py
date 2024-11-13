1decdb3c95302260ffb798a881961126d67d207e
Savana Jones <119756007+savana-jones@users.noreply.github.com>
2024-11-13 20:04:55 +0530

    Update user.py

--- models/user.py
+++ models/user.py
@@ -1,6 +1,6 @@
 class Member:
     def __init__(self, name):
         self.name = name
-
+#Observer pattern
     def update(self, book):
         print(f"{self.name} has been notified that {book.title} is available.")
